269c835a6da261ed038a6e16d7a01ba72c21dede
LJ Spencer <hockeyduck3@gmail.com>
2020-07-22 03:56:25 -0600

    Made some more css changes and added links to the footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,8 +5,22 @@ import './style.css';
 function Footer() {
     return (
         <footer>
-            <p>&copy; Copyright 2020.</p>
-            <p>Made with &hearts; in SLC.</p>
+            <p id='copy'>&copy; Copyright 2020.</p>
+            <p id='madeWith'>Made with &hearts; in SLC.</p>
+            
+            {/* Twitter Link */}
+            <a href='https://twitter.com/ljspencer97' target='_blank' rel='noopener noreferrer'>
+                <i id='twitter' className='fab fa-twitter-square'></i>
+            </a>
+
+            <a id='github' href='https://github.com/hockeyduck3' target='_blank' rel='noopener noreferrer'>
+                <i className='fab fa-github-square'></i>
+            </a>
+
+            <a id='linkedin' href='https://linkedin.com/in/lj-spencer' target='_blank' rel='noopener noreferrer'>
+                <i className='fab fa-linkedin'></i>
+            </a>
+
         </footer>
     )
 }
