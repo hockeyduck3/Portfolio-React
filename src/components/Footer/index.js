873ca3b81fa2c2b3f63fd9555e5a353c8d0e8226
LJ Spencer <hockeyduck3@gmail.com>
2020-07-23 01:45:24 -0600

    Made some navbar changes, and made footer responsive.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,20 +7,23 @@ function Footer() {
         <footer>
             <p id='copy'>&copy; Copyright 2020.</p>
             <p id='madeWith'>Made with &hearts; in SLC.</p>
-            
-            {/* Twitter Link */}
-            <a href='https://twitter.com/ljspencer97' target='_blank' rel='noopener noreferrer'>
-                <i id='twitter' className='fab fa-twitter-square'></i>
-            </a>
 
-            <a id='github' href='https://github.com/hockeyduck3' target='_blank' rel='noopener noreferrer'>
-                <i className='fab fa-github-square'></i>
-            </a>
+            <div className='footerSocial'>
+                {/* LinkedIn Link */}
+                <a id='linkedin' href='https://linkedin.com/in/lj-spencer' target='_blank' rel='noopener noreferrer'>
+                    <i className='fab fa-linkedin'></i>
+                </a>
 
-            <a id='linkedin' href='https://linkedin.com/in/lj-spencer' target='_blank' rel='noopener noreferrer'>
-                <i className='fab fa-linkedin'></i>
-            </a>
+                {/* Github Link */}
+                <a id='github' href='https://github.com/hockeyduck3' target='_blank' rel='noopener noreferrer'>
+                    <i className='fab fa-github-square'></i>
+                </a>
 
+                {/* Twitter Link */}
+                <a href='https://twitter.com/ljspencer97' target='_blank' rel='noopener noreferrer'>
+                    <i id='twitter' className='fab fa-twitter-square'></i>
+                </a>
+            </div>
         </footer>
     )
 }
