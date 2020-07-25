735c9f62eb04ff27b974445488c763fe8fbd1968
LJ Spencer <hockeyduck3@gmail.com>
2020-07-25 02:45:20 -0600

    Added form components to the contact page

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import Container from '../../components/Container/index';
 import Card from '../../components/Card/index';
 
+import './contact.css';
+
 class Contact extends Component {
     state = {
         title: 'Contact Me',
@@ -16,13 +18,83 @@ class Contact extends Component {
         this.setState({ [name]: value });
     }
 
+    validateFunc = event => {
+        event.preventDefault();
+    }
+
     render() {
         return (
             <Container fluid='-fluid'>
                 <Card title={this.state.title}>
                     <div className='mainCard'>
-                        <form id='my-form' action='https://formspree.io/moqngqye' method='POST'>
-                            {/* Add contact form here */}
+                        <form id='my-form' action='https://formspree.io/moqngqye' method='POST' onSubmit={this.validateFunc}>
+
+                            {/* Name */}
+                            <div className='form-group'>
+                                <label className='col-sm-12 control-label' htmlFor='name'>
+                                    Name <span id='nameError' className='hide'>* Cannot contain numbers.</span>
+                                </label>
+
+                                <div className='col-md-10'>
+                                    <input 
+                                        id='name' 
+                                        className='form-control'
+                                        name='name' 
+                                        type='text' 
+                                        placeholder='Your Name'
+                                        onChange={this.inputFunc}
+                                        value={this.state.name}
+                                    />
+                                </div>
+                            </div>
+
+                            {/* Email */}
+                            <div className='form-group'>
+                                <label className='col-sm-12 control-label' htmlFor='email'>
+                                    Email <span id='emailError' className='hide'>* Invalid email.</span>
+                                </label>
+
+                                <div className='col-md-10'>
+                                    <input 
+                                        id='email'
+                                        className='form-control' 
+                                        name='email' 
+                                        type='text' 
+                                        placeholder='Your Email'
+                                        onChange={this.inputFunc}
+                                        value={this.state.email}
+
+                                    />
+                                </div>
+                            </div>
+
+                            {/* Message */}
+                            <div className='form-group'>
+                                <label className='col-sm-12 control-label' htmlFor='message'>
+                                    Message <span id='messageError' className='hide'>* Cannot be empty.</span>
+                                </label>
+
+                                <div className='col-md-10'>
+                                    <textarea 
+                                        id='message' 
+                                        className='form-control' 
+                                        name='message' 
+                                        placeholder='Enter your message here.' 
+                                        rows='3'
+                                        onChange={this.inputFunc}
+                                        value={this.state.message}
+                                    />
+                                </div>
+                            </div>
+
+                            {/* Submit button */}
+                            <div className='form-group'>
+                                <div className='col-md-12 text-right'>   
+                                    <p className='axiousError hide'>Something went wrong...</p>
+                                    <button id='my-form-button' className='btn btn-primary btn-lg' type='submit'>Submit</button>
+                                </div>
+                            </div>
+
                         </form>
                     </div>
                 </Card>
