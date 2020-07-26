9cf8f2ecbcb2efb1a696083f602c16708344752d
LJ Spencer <hockeyduck3@gmail.com>
2020-07-25 22:56:25 -0600

    Made some changes to the card component, and added axios functionality to the contact page.

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,17 +3,28 @@ import React from 'react';
 import './style.css';
 
 function Card(props) {
-    return (
-        <div className='card' id='card'>
-            <div className='card-body'>
-                <h2 className='cardTitle'>{props.title}</h2>
-
-                <hr className='line'></hr>
-
-                {props.children}
+    if (props.title !== 'messageSent') {
+        return (
+            <div className='card' id='card'>
+                <div className='card-body'>
+                    <h2 className='cardTitle'>{props.title}</h2>
+    
+                    <hr className='line'></hr>
+    
+                    {props.children}
+                </div>
             </div>
-        </div>
-    )
+        )
+    } else {
+        return (
+            <div className='card' id='card'>
+                <div className='card-body'>    
+                    {props.children}
+                </div>
+            </div>
+        )
+    }
+
 }
 
 export default Card;
--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Container from '../../components/Container/index';
 import Card from '../../components/Card/index';
@@ -11,7 +12,8 @@ class Contact extends Component {
         title: 'Contact Me',
         name: '',
         email: '',
-        message: ''
+        message: '',
+        status200: false
     }
 
     componentDidMount() {
@@ -109,64 +111,146 @@ class Contact extends Component {
     }
 
     sendEmail = () => {
-        // send email to email API
+        
+        const form = document.getElementById('my-form');
+        
+        const data = new FormData(form);
+        
+        document.getElementById('card').classList.add('animate__animated', 'animate__bounceOutUp');
+
+        setTimeout(() => {
+            axios({
+                method: 'post',
+                url: 'https://formspree.io/moqngqye',
+                data: data
+            }).then(res => {
+                this.setState({status200: true});
+
+                this.emailSuccess();
+            }).catch(err => {
+                this.emailFail();
+
+                console.log(err);
+            });
+        }, 1500);
+
+    }
+
+    emailSuccess = () => {
+        const card = document.getElementById('card').classList;
+
+        card.replace('animate__bounceOutUp', 'animate__bounceInDown');
+
+        setTimeout(() => {
+            card.replace('animate__bounceInDown', 'animate__flipOutY');
+
+            setTimeout(() => {
+                this.setState({
+                    name: '',
+                    email: '',
+                    message: '',
+                    status200: false
+                });
+
+                card.replace('animate__flipOutY', 'animate__flipInY');
+
+                setTimeout(() => {
+                    card.remove('animate__animated', 'animate__flipInY');
+
+                }, 1000);
+            }, 1000);
+        }, 2500);
+    }
+
+    emailFail = () => {
+        const card = document.getElementById('card').classList;
+
+        const error = document.querySelector('.axiosError').classList;
+
+        card.replace('animate__bounceOutUp', 'animate__bounceInDown');
+
+        error.remove('hide');
+        setTimeout(() => {
+            card.remove('animate__animated', 'animate__bounceInDown');
+        }, 1000);
+
+        setTimeout(() => {
+
+            error.add('animate__animated', 'animate__fadeOut');
+            
+            setTimeout(() => {
+                error.remove('animate__animated', 'animate__fadeOut');
+                error.add('hide');
+            }, 1000);
+        }, 3000);
     }
 
     render() {
+        const status = this.state.status200;
+
         return (
             <Container fluid='-fluid'>
-                <Card title={this.state.title}>
-                    <div className='mainCard'>
-                        <form id='my-form' action='https://formspree.io/moqngqye' method='POST' onSubmit={this.validateFunc}>
+                {!status ? (
+                    <Card title={this.state.title}>
+                        <div className='mainCard'>
+                                <form id='my-form' onSubmit={this.validateFunc}>
 
-                            {/* Name */}
-                            <FormGroup name='name' label='Name' errorMessage='* Cannot contain numbers.'>
-                                <input 
-                                    id='name' 
-                                    className='form-control'
-                                    name='name' 
-                                    type='text' 
-                                    placeholder='Your Name'
-                                    onChange={this.inputFunc}
-                                    value={this.state.name}
-                                />
-                            </FormGroup>
+                                    {/* Name */}
+                                    <FormGroup name='name' label='Name' errorMessage='* Cannot contain numbers.'>
+                                        <input 
+                                            id='name' 
+                                            className='form-control'
+                                            name='name' 
+                                            type='text' 
+                                            placeholder='Your Name'
+                                            onChange={this.inputFunc}
+                                            value={this.state.name}
+                                        />
+                                    </FormGroup>
 
-                            {/* Email */}
-                            <FormGroup name='email' label='Email' errorMessage='* Invalid email.'>
-                                <input 
-                                    id='email'
-                                    className='form-control' 
-                                    name='email' 
-                                    type='text' 
-                                    placeholder='Your Email'
-                                    onChange={this.inputFunc}
-                                    value={this.state.email}
-                                />
-                            </FormGroup>
+                                    {/* Email */}
+                                    <FormGroup name='email' label='Email' errorMessage='* Invalid email.'>
+                                        <input 
+                                            id='email'
+                                            className='form-control' 
+                                            name='email' 
+                                            type='text' 
+                                            placeholder='Your Email'
+                                            onChange={this.inputFunc}
+                                            value={this.state.email}
+                                        />
+                                    </FormGroup>
 
-                            {/* Message */}
-                            <FormGroup name='message' label='Message' errorMessage='* Cannot be empty.'>
-                                <textarea 
-                                    id='message' 
-                                    className='form-control' 
-                                    name='message' 
-                                    placeholder='Enter your message here.' 
-                                    rows='3'
-                                    onChange={this.inputFunc}
-                                    value={this.state.message}
-                                />
-                            </FormGroup>
+                                    {/* Message */}
+                                    <FormGroup name='message' label='Message' errorMessage='* Cannot be empty.'>
+                                        <textarea 
+                                            id='message' 
+                                            className='form-control' 
+                                            name='message' 
+                                            placeholder='Enter your message here.' 
+                                            rows='3'
+                                            onChange={this.inputFunc}
+                                            value={this.state.message}
+                                        />
+                                    </FormGroup>
 
-                            {/* Submit button */}
-                            <FormGroup name='submit'>
-                                <p className='axiousError hide'>Something went wrong...</p>
-                                <button id='my-form-button' className='btn btn-primary btn-lg' type='submit'>Submit</button>
-                            </FormGroup>
+                                    {/* Submit button */}
+                                    <FormGroup name='submit'>
+                                        <p className='axiosError hide'>Something went wrong...</p>
+                                        <button id='my-form-button' className='btn btn-primary btn-lg' type='submit'>Submit</button>
+                                    </FormGroup>
 
-                        </form>
-                    </div>
-                </Card>
+                                </form>
+                            </div>
+                    </Card>
+
+                ) : (
+                    <Card title='messageSent'>
+                        <p id='messageSent'>Message Sent!</p>
+                    </Card>
+                )}
+
+                <p id='emailText'>If you don't want to use the above contact form, you can reach out to me directly at <em><a href='mailTo:hockeyduck3@gmail.com'>hockeyduck3@gmail.com</a></em></p>
             </Container>
         )
     }
