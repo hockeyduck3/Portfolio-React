3a28326d48c97962a8543e03465e972ea315c0d6
LJ Spencer <hockeyduck3@gmail.com>
2020-07-28 07:28:50 -0600

    Changed router.get to server.get

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const router = require('express').Router();
 
 const server = express();
 
@@ -13,10 +12,10 @@ if (process.env.NODE_ENV === 'production') {
     server.use(express.static('client/build'));
 }
 
-router.get('*', (req, res) => {
+server.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
 server.listen(PORT, () => {
-    console.log(`Listening on localhost:${PORT}`)
+    console.log(`Listening on localhost:${PORT}`);
 });
