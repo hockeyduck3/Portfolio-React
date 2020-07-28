e735ccb8f37447fd44dbec131e7c7859dc21f480
LJ Spencer <hockeyduck3@gmail.com>
2020-07-28 17:07:09 -0600

    SImplified server.js file

--- server.js
+++ server.js
@@ -5,16 +5,7 @@ const server = express();
 
 const PORT = process.env.PORT || 3001;
 
-server.use(express.urlencoded({ extended: true }));
-server.use(express.json);
-
-if (process.env.NODE_ENV === 'production') {
-    server.use(express.static('client/build'));
-}
-
-server.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './client/build/index.html'));
-});
+server.get('*', (req, res) => res.send('Hello World!'))
 
 server.listen(PORT, () => {
     console.log(`Listening on localhost:${PORT}`);
