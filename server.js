fa957c62ddc6826091897d9fc5dc8b36f42dee2b
LJ Spencer <hockeyduck3@gmail.com>
2020-07-28 19:31:08 -0600

    Fixed server.js file

--- server.js
+++ server.js
@@ -3,16 +3,15 @@ const path = require('path');
 
 const server = express();
 
-const PORT = process.env.PORT || 3001;
+// Serve the static files from the React server
+server.use(express.static(path.join(__dirname, 'client/build')));
 
-if (process.env.NODE_ENV === 'production') {
-    server.use(express.static('client/build'));
-}
-
-server.get('*', (req, res) => {
+// Handles any requests that don't match the ones above
+server.get('*', (req,res) =>{
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
-server.listen(PORT, () => {
-    console.log(`Listening on localhost:${PORT}`);
-});
+const port = process.env.PORT || 3001;
+server.listen(port);
+
+console.log('App is listening on port ' + port);
