97d1255a8b0553590bbc73a8ec905a22bb71cbb8
LJ Spencer <hockeyduck3@gmail.com>
2020-07-28 19:25:42 -0600

    Readded the if statement just to test something

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const server = express();
 
 const PORT = process.env.PORT || 3001;
 
-server.use(express.static(path.join(__dirname, 'client/build')));
+if (process.env.NODE_ENV === 'production') {
+    server.use(express.static('client/build'));
+}
 
 server.get('*', (req, res) => {
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
