8f362829f293db4c07ec5266fdb181a02bc641b9
LJ Spencer <hockeyduck3@gmail.com>
2020-07-28 19:08:47 -0600

    Remove the if statement from the server.js file

--- server.js
+++ server.js
@@ -8,9 +8,7 @@ const PORT = process.env.PORT || 3001;
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json);
 
-if (process.env.NODE_ENV === 'production') {
-    server.use(express.static('client/build'));
-}
+server.use(express.static(path.join(__dirname, 'client/build')));
 
 server.get('*', (req, res) => {
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
