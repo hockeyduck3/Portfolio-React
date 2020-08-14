b9574981b2b0bfe312601c5dcb8e93f6c8c54f2f
LJ Spencer <hockeyduck3@gmail.com>
2020-08-14 15:14:39 -0600

    Added funcitonality so when the navbar is open an X will appear instead of the Hamburger menu.

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,18 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink, Link } from 'react-router-dom';
 
 import Resume from '../../PDF/LJ-Spencer-Resume-2020.pdf';
 import './style.css';
 
 function Nav() {
+    const [navbarToggleBtn, setNavbarToggleBtn] = useState(false);
+
     const body = document.body;
     const navbar = document.getElementsByClassName('collapsed');
     
     body.addEventListener('click', function() {
         if (navbar.length === 0) {
             document.querySelector('.navbar-toggler').click();
+
+            if (navbarToggleBtn) {
+                changeFunc();
+            }
         }
     });
+
+    function changeFunc() {
+        const navbarBtn = document.getElementById('navbar-toggler');
+
+        if (!navbarToggleBtn) {
+            navbarBtn.style.setProperty('--animate-duration', '0.3s');
+            navbarBtn.classList.add('animate__animated', 'animate__fadeOut');
+    
+            navbarBtn.addEventListener('animationend', () => {
+                setNavbarToggleBtn(true);
+                navbarBtn.classList.replace('animate__fadeOut', 'animate__fadeIn');
+
+                setTimeout(() => {
+                    navbarBtn.classList.remove('animate__animated', 'animate__fadeIn');
+                }, 500);
+            });
+        } else {
+            navbarBtn.classList.add('animate__animated', 'animate__fadeOut');
+    
+            navbarBtn.addEventListener('animationend', () => {
+                setNavbarToggleBtn(false);
+                navbarBtn.classList.replace('animate__fadeOut', 'animate__fadeIn');
+
+                setTimeout(() => {
+                    navbarBtn.classList.remove('animate__animated', 'animate__fadeIn');
+                }, 500);
+            });
+        }
+    }
 
     return (
         <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
@@ -22,8 +57,24 @@ function Nav() {
                 LJ Spencer
             </Link>
 
-            <button id='navbarBtn' className='navbar-toggler collapsed' type='button' data-toggle='collapse' data-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
-                <span className='navbar-toggler-icon'></span>
+            <button 
+                id='navbarBtn' 
+                className='navbar-toggler collapsed' 
+                type='button' data-toggle='collapse' 
+                data-target='#navbarNav' aria-controls='navbarNav' 
+                aria-expanded='false' 
+                aria-label='Toggle navigation'
+            >
+                {!navbarToggleBtn ? (
+                    <span id='navbar-toggler' className='navbar-toggler-icon' onClick={changeFunc}></span>
+                ) : (
+                    <span id='navbar-toggler' onClick={changeFunc} style={{
+                        display: 'inline-block',
+                        width: '1.5em',
+                        height: '1.5em',
+                        verticalAlign: 'middle'
+                    }}><i className='fas fa-times fa-lg'></i></span>
+                )}
             </button>
 
             <div className='collapse navbar-collapse' id='navbarNav'>
