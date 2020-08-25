30429b3bbe352daec2b0e0992e3941a23ebe9041
LJ Spencer <hockeyduck3@gmail.com>
2020-08-24 20:13:55 -0600

    Added onImagesLoaded to the 404 page

--- client/src/pages/404/index.js
+++ client/src/pages/404/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import OnImagesLoaded from 'react-on-images-loaded';
 
 import Container from '../../components/Container/index';
 import Card from '../../components/Card/index';
@@ -9,31 +10,45 @@ function NotFound() {
     useEffect(() => {
         document.querySelector('.navbar').style.display = 'none';
         document.querySelector('footer').style.display = 'none';
+        document.getElementById('notFoundCard').style.display = 'none'
     }, []);
+
+    function loaded() {
+        const card = document.getElementById('notFoundCard');
+
+        card.classList.add('animate__animated', 'animate__slideInDown');
+        card.style.display = 'block';
+    }
 
     return (
         <Container>
-            <div className='row'>
-                <Card title='404' id='notFoundCard'>
-                    <h2 id='notFoundTitle'>The page you're looking for couldn't be found... <span role='img' aria-label='One Tear Emoji'>😢</span></h2>
+            <OnImagesLoaded
+                onLoaded={() => loaded()}
+                onTimeout={() => loaded()}
+                timeout={10000}
+            >
+                <div className='row'>
+                    <Card title='404' id='notFoundCard'>
+                        <h2 id='notFoundTitle'>The page you're looking for couldn't be found... <span role='img' aria-label='One Tear Emoji'>😢</span></h2>
 
-                    <div id='sadDiv'>
-                        <img id='sadGif' src='./images/sad.gif' alt='Saddnes from Inside Out is crying because you got a 404 error.' />
-                    </div>
+                        <div id='sadDiv'>
+                            <img id='sadGif' src='./images/sad.gif' alt='Saddnes from Inside Out is crying because you got a 404 error.' />
+                        </div>
 
-                    <p className='notFoundText'>
-                        Please make sure you typed the link correctly.
-                    </p>
+                        <p className='notFoundText'>
+                            Please make sure you typed the link correctly.
+                        </p>
 
-                    <p className='notFoundText'>
-                        Or if this was a glitch please contact LJ at <em><a href='mailTo:hockeyduck3@gmail.com'>hockeyduck3@gmail.com</a></em>
-                    </p>
+                        <p className='notFoundText'>
+                            Or if this was a glitch please contact LJ at <em><a href='mailTo:hockeyduck3@gmail.com'>hockeyduck3@gmail.com</a></em>
+                        </p>
 
-                    <p className='notFoundText'>
-                        <a href='/'>Click here to go back to the homepage.</a>
-                    </p>
-                </Card>
-            </div>
+                        <p className='notFoundText'>
+                            <a href='/'>Click here to go back to the homepage.</a>
+                        </p>
+                    </Card>
+                </div>
+            </OnImagesLoaded>
         </Container>
     )
 }
