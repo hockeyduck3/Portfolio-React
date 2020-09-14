a71924805fac8a48d594a850ee3b6a0e8f3fa434
LJ Spencer <hockeyduck3@gmail.com>
2020-09-14 15:40:57 -0600

    Fixed some styling errors with the React logo

--- client/src/pages/Homepage/index.js
+++ client/src/pages/Homepage/index.js
@@ -123,7 +123,7 @@ class Homepage extends Component {
 
                 <div id='githubLink'>
                     <a href='https://github.com/hockeyduck3/Portfolio-React' target='_blank' rel='noopener noreferrer'>Click here for my website's Repo <i className='fab fa-github' /></a>
-                    <p id='homepageReactLink'>My portfolio was built using <a id='reactLink' href='https://reactjs.org/' target='_blank' rel='noopener noreferrer'>React <i className='fab fa-react reactLinkLogo' /></a></p>
+                    <p id='homepageReactLink'>My portfolio was built using <a id='reactLink' href='https://reactjs.org/' target='_blank' rel='noopener noreferrer'>React <i className='fab fa-react' id='reactLinkLogo' /></a></p>
                 </div>
             </Container>
         )
